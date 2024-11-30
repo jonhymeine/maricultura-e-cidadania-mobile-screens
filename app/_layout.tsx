c34a6b4a8ce6e5a616acd42ffe7c11735374339d
Jonathas Meine <jonhy.meine@gmail.com>
2024-11-30 15:18:05 -0300

    styles: remove semicolons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import 'react-native-reanimated'
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter'
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded] = useFonts({
--- styles/screens/home.ts
+++ styles/screens/home.ts
@@ -1,5 +1,5 @@
-import { StyleSheet } from "react-native";
-import { Theme } from "../Theme";
+import { StyleSheet } from "react-native"
+import { Theme } from "../Theme"
 
 export const styles = StyleSheet.create({
   container: {
