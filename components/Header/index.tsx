944477d73ab70e60b1647cab42dd729b206bd2f8
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-11-30 17:39:14 -0300

    feat: add `Link` to navigate user to the `Social` screen

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React, { memo } from 'react';
+import { Link } from 'expo-router';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { UNIVALI_LOGO } from '../../config';
 import { headerStyles } from '../../styles';
@@ -11,9 +12,11 @@ function Header() {
 
       <Text style={headerStyles.text}>Maricultura e Cidadania</Text>
 
-      <TouchableOpacity activeOpacity={0.8}>
-        <Icon variant='list' color='dark-blue' size='large' />
-      </TouchableOpacity>
+      <Link href={'/social'} asChild>
+        <TouchableOpacity activeOpacity={0.8}>
+          <Icon variant='list' color='dark-blue' size='large' />
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
